70567a33238b9deaea202253f7a2d49de7348ea7
Ubair Javid <enunmuz@users.noreply.github.com>
2022-10-12 00:55:50 +0530

    more ui added

--- assets/js/customFn.js
+++ assets/js/customFn.js
@@ -1,40 +1,53 @@
-//eventListener to select the file
-// document
-//   .getElementById("uploadBgImg")
-//   .addEventListener("change", readURL, true);
-
-//function to select images only
 function loadFile(event) {
   var src = URL.createObjectURL(event.target.files[0]);
-
-  // var file = document.getElementById("uploadBgImg").files[0];
-  // var reader = new FileReader();
-  // reader.onloadend = function () {
-  //   console.log(reader);
   $(".activeThumb").css("background-image", "url(" + src + ")");
-  // };
-  // if (file) {
-  //   reader.readAsDataURL(file);
-  // } else {
-  // }
+  var canvasSrc = $(".activeThumb").css("background-image");
+  canvasSrc = canvasSrc
+    .replace("url(", "")
+    .replace(")", "")
+    .replace(/\"/gi, "");
+  $("#qwe").css("background-image", "url(" + canvasSrc + ")");
 }
 
 function selectThumb(elem) {
   $(".app-top-section-menu-thumb-container").removeClass("activeThumb");
   $(elem).addClass("activeThumb");
-  var src = $(".activeThumb").css("background-image");
-  src = src.replace("url(", "").replace(")", "").replace(/\"/gi, "");
-  $("#qwe").css("background-image", "url(" + src + ")");
+  var canvasSrc = $(".activeThumb").css("background-image");
+  canvasSrc = canvasSrc
+    .replace("url(", "")
+    .replace(")", "")
+    .replace(/\"/gi, "");
+  $("#qwe").css("background-image", "url(" + canvasSrc + ")");
 }
 
 function addDisplay(elem) {
-  $(`<div class="app-top-section-menu-delay-container">
+  console.log($(".app-top-section-menu-thumb-container").length);
+  if ($(".app-top-section-menu-thumb-container").length == 0) {
+    $(
+      `<div class="app-top-section-menu-thumb-container"  onclick="selectThumb(this);">  <span class="remove-menu-thumb" onclick="removeDisplay(this);"> </span></div>`
+    ).insertBefore(".app-top-section-menu-add-container");
+  } else {
+    $(`<div class="app-top-section-menu-delay-container">
     <div class="app-top-section-menu-delay-label">Delay</div>
     <div class="app-top-section-menu-delay-input-secs">
       <input type="number" name="" id="delayInput" min="1" value="5" />
     </div>
     <div class="app-top-section-menu-delay-secs-label">Secs</div>
-  </div><div class="app-top-section-menu-thumb-container"  onclick="selectThumb(this);"></div>`).insertBefore(
-    ".app-top-section-menu-add-container"
-  );
+  </div><div class="app-top-section-menu-thumb-container"  onclick="selectThumb(this);">  <span class="remove-menu-thumb" onclick="removeDisplay(this);"> </span></div>`).insertBefore(
+      ".app-top-section-menu-add-container"
+    );
+  }
+}
+function removeDisplay(elem) {
+  if (
+    $(elem)
+      .parent()
+      .prev("div")
+      .hasClass("app-top-section-menu-delay-container")
+  ) {
+    $(elem).parent().prev("div").remove();
+    $(elem).parent().remove();
+  } else {
+    $(elem).parent().remove();
+  }
 }
